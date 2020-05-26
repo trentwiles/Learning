96aa1fc99b989926304b7ea7e0123adf5f30f262
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2020-05-26 17:54:29 -0400

    Variables

---
+++ variables.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  string characterName = "Tom";
+  int characterAge;
+  characterAge = 50;
+  cout << "There once was a man named " << characterName << endl;
+  cout << "He was " << characterName << " years old" << endl;
+  characterName = "Mike";
+  cout << "He liked the name " << characterName << endl;
+  cout << "But did not like being " << characterAge << endl;
+  
+  return 0;
+}
