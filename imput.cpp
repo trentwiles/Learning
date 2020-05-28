5d194dd9dd3962b353e6d7893f2b35c62806896c
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2020-05-28 14:00:45 -0400

    Imput

---
+++ imput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+  string name;
+  cout << "Enter your name: " <<;
+  getLine(cin, name);
+  
+  cout << "Hello " << name;
+
+  return 0;
+}
