23c3ae7e4189635d759a73beb81df9ee092f401d
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2020-05-28 14:22:25 -0400

    Calculator

---
+++ calc.cpp
@@ -0,0 +1,19 @@
+#inlcude <iostream>
+#inlcude <cmath>
+
+using namespace std;
+
+int main()
+{
+  double num1, num2;
+  
+  cout << "Enter first number: ";
+  cin >> num1;
+  
+  cout << "Enter second number: ";
+  cin >> num2;
+  
+  cout << num1 + num2;
+  
+  return 0;
+}
