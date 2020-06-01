fb263bf9386c29981b10ae818816f3ef8bfdd183
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2020-06-01 17:52:50 -0400

    Switches

---
+++ switches.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int day = 2;
+	switch (day) {
+	case 1:
+		cout << "Monday";
+		break;
+	case 2:
+		cout << "Tuesday";
+		break;
+	case 3:
+		cout << "Wedsday";
+		break;
+	case 4:
+		cout << "Thursday";
+		break;
+	case 5:
+		cout << "Friday";
+		break;
+	case 6:
+		cout << "Saturday";
+		break;
+	case 7:
+		cout << "Sunday";
+		break;
+	}
+
+	return 0;
+}
