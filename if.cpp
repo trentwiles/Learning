11bffad7e6fd39a85587876d1a28c1bc11dc2010
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2020-06-01 17:47:32 -0400

    If/Else

---
+++ if.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int x = 10;
+	int y = 20;
+
+	if (x > y){
+		cout << "What you just put in is true!";
+	}
+	else {
+		cout << "Sorry! That does not appear to be true. :(";
+	}
+
+	return 0;
+}
