465f200fc0c6c50c308e4a31fe8a905a50c3d03f
RiversideRocks <59586759+RiversideRocks@users.noreply.github.com>
2020-06-11 09:52:46 -0400

    Added namespace RiversideRocks

--- namespace.cpp
+++ namespace.cpp
@@ -6,13 +6,22 @@ using namespace std;
 
 namespace tucker {
     void bark() {
-        cout << "Tucker is cool.";
+        cout << "Tucker is cool. \n";
+    }
+}
+
+namespace riversiderocks{
+    void hello(){
+        cout << "Riverside is cool!";
     }
 }
 
 using namespace tucker;
+using namespace riversiderocks;
+
 
 int main()
 {
     bark();
+    hello();
 }
